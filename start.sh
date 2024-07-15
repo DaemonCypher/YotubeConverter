46a4db79145a1163b6d65591279958ff798834a3
DaemonCypher <daemoncypher00@gmail.com>
2024-07-15 13:27:12 -0500

    testing replit

--- start.sh
+++ start.sh
@@ -6,9 +6,8 @@ uvicorn app:app --host=0.0.0.0 --port=8080 &
 # Wait for FastAPI backend to start
 sleep 5
 
-# Change directory to Vue.js frontend
+# Change directory to Vue.js frontend (if needed)
 cd YoutubeConverter
 
 # Start Vue.js frontend
 npm run serve
-
